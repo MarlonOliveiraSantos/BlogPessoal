d70a4ca7846a3694d3f7dedaa69cb9f5cdaa9661
marlon <marlon.oliveira34@hotmail.com>
2023-10-02 10:00:30 -0300

    segundo commit

--- src/main/java/com/example/Blog/Pessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/Blog/Pessoal/repository/PostagemRepository.java
@@ -1,10 +1,16 @@
 package com.example.Blog.Pessoal.repository;
 
 import com.example.Blog.Pessoal.model.Postagem;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
